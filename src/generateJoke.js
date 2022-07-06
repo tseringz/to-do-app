af3240f8f964cd9b523156a69f4a34f8ccb1083d
Tsering Dhondup <tserockon77@hotmail.com>
2022-07-06 18:44:49 +0530

    Create webpack environment for the app

---
+++ src/generateJoke.js
@@ -0,0 +1,5 @@
+function generateJoke() {
+    return "I don't trust this guy to be trusted";
+}
+
+export default generateJoke;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import generateJoke from './generateJoke';
+import './style/main.scss';
+
+console.log(generateJoke());
+console.log("this is not me and you are not allowed to create a new instance of this class");
